0202e4c2fce57e857c08ce8e5289b6b8ecdf4d09
HongJun <hongjun@HongJuns-MacBook-Pro.local>
2025-08-09 23:02:09 +0800

    app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -4,26 +4,18 @@ import { startApolloServer } from "./src/config/apolloServer";
 import { EventListener } from "./src/utils/events/EventListener";
 import { jobElasticService } from "./src/utils/elasticSearch/job.elasticSearch";
 import { elasticClient } from "./src/config/elasticSearch";
-import { JOB_EVENTS } from "./src/constant/constant";
 
 
-
-
-dotenv.config();
-
-// Start Apollo Server
-startApolloServer();
 
 (async () => {
+  await startApolloServer();
+
   try {
     const info = await elasticClient.info();
     console.log("Connected to Elasticsearch:", info);
   } catch (err) {
     console.error("Elasticsearch connection failed:", err);
   }
+
+  EventListener.getInstance().listen(jobElasticService);
 })();
-
-// Event handler
-// Initialize Event Listener (singleton)
-EventListener.getInstance().listen(jobElasticService);
-
